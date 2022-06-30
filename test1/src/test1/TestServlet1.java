7eca35ccc1a93816667e5127bbf3c6a7e252df4c
user <user@WIN10JAVA>
2022-06-30 13:35:17 +0900

    2nd

--- test1/src/test1/TestServlet1.java
+++ test1/src/test1/TestServlet1.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class TestServlet1
+ * Servlet implementation class TestServlet1 V1.0
  */
 public class TestServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
